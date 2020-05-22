aab43b2c323e8fdffdee1f41cbe44f25a4a61c1c
martin <martdevelopers254@gmail.com>
2020-05-22 16:33:25 +0300

    Implemented Dashboard Partials

--- resources/views/Core/Admin/config/checklogin.php
+++ resources/views/Core/Admin/config/checklogin.php
@@ -1,41 +1,17 @@
 <?php
 function check_login()
 {
-	if(strlen($_SESSION['c_id'])==0)
+	if(strlen($_SESSION['a_id'])==0)
 		{
 			$host = $_SERVER['HTTP_HOST'];
 			$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-			$extra="../views/";
-			$_SESSION["c_id"]="";
+			$extra="../Admin/";
+			$_SESSION["a_id"]="";
 			header("Location: http://$host$uri/$extra");
 			
 		}
-	
 }
 
-function sudoChecklogin()
-{
-	if(strlen($_SESSION['a_id'])==0)
-	{
-		$host = $_SESSION['HTTP_HOST'];
-		$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra = "../";
-		$_SESSION["a_id"] = "";
-		header("Location: http://$host$uri/$extra");
-	}
-}
-
-function professionChecklogin()
-{
-	if(strlen($_SESSION['p_id'])==0)
-	{
-		$host = $_SESSION['HTTP_HOST'];
-		$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra = "../../profession/";
-		$_SESSION["p_id"] = "";
-		header("Location: http://$host$uri/$extra");
-	}
-}
 
 
 
