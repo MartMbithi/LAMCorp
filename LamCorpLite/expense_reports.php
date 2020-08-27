4b7bc3bc5756932182a79cbb7c574488c1d49810
MartMbithi <martdevelopers254@gmail.com>
2020-08-27 05:33:16 +0300

    Multiple Bug Fixes On Expense Report Data Table

---
+++ LamCorpLite/expense_reports.php
@@ -0,0 +1,175 @@
+<?php
+session_start();
+include('config/config.php');
+include('config/checklogin.php');
+check_login();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Water Management System - Payment Reports</title>
+
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="vendor/DT/datatables.css">
+    <link rel="stylesheet" type="text/css" href="vendor/DT/custom_dt_html5.css">
+    <link rel="stylesheet" type="text/css" href="vendor/DT/dt-global_style.css">
+
+</head>
+
+<body id="page-top">
+
+    <?php require_once('partials/_nav.php'); ?>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php
+        require_once('partials/_sidebar.php')
+        ?>
+
+        <div id="content-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Breadcrumbs-->
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        <a href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="breadcrumb-item">Reports</li>
+                    <li class="breadcrumb-item Active">Payments</li>
+
+                </ol>
+
+                <!-- DataTables Example -->
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h5>Expense Reports</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="html5-extension" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>Number</th>
+                                        <th>Till Number</th>
+                                        <th>Trn Code</th>
+                                        <th>Ref No</th>
+                                        <th>Amount</th>
+                                        <th>Date Paid</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $ret = "SELECT * FROM  Payments  ";
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute();
+                                    $res = $stmt->get_result();
+                                    while ($k = $res->fetch_object()) {
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $k->Payment_number; ?></td>
+                                            <td><?php echo $k->Payment_till_number; ?></td>
+                                            <td><?php echo $k->Payment_transaction_code; ?></td>
+                                            <td><?php echo $k->Payment_reference_no; ?></td>
+                                            <td>Ksh <?php echo $k->Payment_amount; ?></td>
+                                            <td><?php echo $k->Payment_date; ?></td>
+
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="card-footer small text-muted">Generated on <?php echo date('d M Y '); ?> at <?php echo date('g:i'); ?></div>
+                </div>
+            </div>
+            <!-- /.container-fluid -->
+
+            <!-- Sticky Footer -->
+            <?php
+            require_once('partials/_footer.php');
+            ?>
+
+        </div>
+        <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <?php
+    require_once('partials/_logout.php');
+    ?>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/DT/jquery.dataTables.js"></script>
+    <script src="vendor/DT/dataTables.bootstrap4.js"></script>
+    <script src="vendor/DT/datatables.js"></script>
+    <!-- NOTE TO Use Copy CSV Excel PDF Print Options You Must Include These Files  -->
+    <script src="vendor/DT/button-ext/dataTables.buttons.min.js"></script>
+    <script src="vendor/DT/button-ext/jszip.min.js"></script>
+    <script src="vendor/DT/button-ext/buttons.html5.min.js"></script>
+    <script src="vendor/DT/button-ext/buttons.print.min.js"></script>
+    <script>
+        $('#html5-extension').DataTable({
+            dom: '<"row"<"col-md-12"<"row"<"col-md-6"B><"col-md-6"f> > ><"col-md-12"rt> <"col-md-12"<"row"<"col-md-5"i><"col-md-7"p>>> >',
+            buttons: {
+                buttons: [{
+                        extend: 'copy',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'csv',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'excel',
+                        className: 'btn'
+                    },
+                    {
+                        extend: 'print',
+                        className: 'btn'
+                    }
+                ]
+            },
+            "oLanguage": {
+                "oPaginate": {
+                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
+                },
+                "sInfo": "Showing page _PAGE_ of _PAGES_",
+                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+                "sSearchPlaceholder": "Search...",
+                "sLengthMenu": "Results :  _MENU_",
+            },
+            "stripeClasses": [],
+            "lengthMenu": [7, 10, 20, 50],
+            "pageLength": 7
+        });
+    </script>
+</body>
+
+</html>
