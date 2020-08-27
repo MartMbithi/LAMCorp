9d4e2e0aa110f5ddbd5419ea9257f1ff41433fdc
MartMbithi <martdevelopers254@gmail.com>
2020-08-27 04:41:03 +0300

    Attached Data Tables Js Version 2.0

--- LamCorpLite/staff_reports.php
+++ LamCorpLite/staff_reports.php
@@ -3,9 +3,27 @@ session_start();
 include('config/config.php');
 include('config/checklogin.php');
 check_login();
-
-require_once('partials/_head.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Water Management System - Instilling Inovation In Water Sales</title>
+
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="vendor/DataTables/datatables.min.css" />
+
+</head>
 
 <body id="page-top">
 
@@ -110,6 +128,7 @@ require_once('partials/_head.php');
 
     <!-- Demo scripts for this page-->
     <script src="js/demo/datatables-demo.js"></script>
+    <script type="text/javascript" src="vendor/DataTables/datatables.min.js"></script>
 
 </body>
 
